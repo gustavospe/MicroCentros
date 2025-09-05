56a58ddb48a4fcc1acea7cbede588289cd52af27
Gustavo Spengler <gustavo.ferreira@guiafacil.com>
2025-09-05 09:49:50 -0300

    Configurando basicas do nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,23 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  srcDir: 'app/web/',
+  pages: true,
+  css: [
+  '@unocss/reset/tailwind.css',
+  '@unocss/reset/normalize.css',
+  '@unocss/reset/eric-meyer.css',
+  '@unocss/reset/sanitize/sanitize.css',
+  '@unocss/reset/sanitize/assets.css',
+  '@/assets/main.css',
+  ],
   modules: [
     '@nuxt/eslint',
     '@nuxt/content',
     '@nuxt/image',
     '@nuxt/scripts',
     '@nuxt/ui',
-    '@nuxt/test-utils'
-  ]
+    '@nuxt/test-utils',
+  '@unocss/nuxt',
+  ],
 })
