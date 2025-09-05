cb1048758fe94ad360c819604273c39d1d6eca91
Gustavo Spengler <gustavo.ferreira@guiafacil.com>
2025-09-05 09:51:48 -0300

    definindo middleware

---
+++ app/middleware/global.ts
@@ -0,0 +1,5 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  // Exemplo de middleware global (pode ser expandido para autenticação)
+  // Se quiser proteger rotas, adicione lógica aqui
+  return true
+})
