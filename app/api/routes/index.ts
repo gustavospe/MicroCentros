305056e8309e3c1777afaff096d2a164e58125a6
Gustavo Spengler <gustavo.ferreira@guiafacil.com>
2025-09-05 11:15:27 -0300

    Remocao do diretorio

--- app/api/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { connectToDatabase } from '../mongo'
-import type { H3Event } from 'h3'
-
-export default defineEventHandler(async (event: H3Event) => {
-  // Exemplo de rota API básica
-  const db = await connectToDatabase()
-  return { status: 'ok', dbs: await db.admin().listDatabases() }
-})
